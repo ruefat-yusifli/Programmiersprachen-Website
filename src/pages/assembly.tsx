34724d91d6390768f946f6910c5f1350ecb734ab
Lukas <r.yusifli@yahoo.com>
2025-06-17 13:33:23 +0200

    added last + next page button for the assembly and cpp page

--- src/pages/assembly.tsx
+++ src/pages/assembly.tsx
@@ -1,4 +1,6 @@
 import "../index.css"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 /**
  * Renders the Assembly page component.
@@ -8,12 +10,39 @@ import "../index.css"
  * @returns {JSX.Element} The rendered Assembly page.
  */
 export function Assembly() {
-    return (
+    const [hoverNext, setHoverNext] = useState(false);
+    const [hoverLast, setHoverLast] = useState(false);
+    const navigate = useNavigate();
+    return (   
         <>
             <div>
                 <h1>Assembly Page</h1>
                 <p>This is the Assembly page content.</p>
             </div>
+             <div className="page">
+                <button
+                    style={{
+                        color: hoverLast ? "black" : "rgb(255, 66, 129)",
+                        fontSize: "18px",
+                        border: "2px solid rgb(255, 66, 129)",
+                        background: hoverLast ? "rgb(255, 66, 129)" : "transparent",
+                        padding: "12px 30px",
+                        borderRadius: "6px",
+                        cursor: "pointer",
+                        transition: "all 0.2s ease",
+                        position: "fixed",
+                        top: "750px",
+                        left: "140px",
+                        fontWeight: "bold",
+                    }}
+                    onMouseEnter={() => setHoverLast(true)}
+                    onMouseLeave={() => setHoverLast(false)}
+                    onClick={() => navigate("/pages/cpp")}>
+                
+                    Last Page
+                </button> {/*This Button navigates to the last page*/}
+            </div>
+ 
         </>
     );
 }
--- src/pages/cpp.tsx
+++ src/pages/cpp.tsx
@@ -1,3 +1,7 @@
+import "../index.css"
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 /**
  * React component that renders the C++ page.
  *
@@ -5,12 +9,60 @@
  *
  * @returns {JSX.Element} The rendered C++ page content.
  */
+
 export function Cpp() {
+    const [hoverNext, setHoverNext] = useState(false);
+    const [hoverLast, setHoverLast] = useState(false);
+    const navigate = useNavigate();
     return (
         <>
             <div>
                 <h1>C++ Page</h1>
                 <p>This is the C++ page content.</p>
+            </div>
+            <div className="page">
+                <button
+                    style={{
+                        color: hoverNext ? "black" : "rgb(255, 66, 66)",
+                        fontSize: "18px",
+                        border: "2px solid rgb(255, 66, 66)",
+                        background: hoverNext ? "rgb(255, 66, 66)" : "transparent",
+                        padding: "12px 30px",
+                        borderRadius: "6px",
+                        cursor: "pointer",
+                        transition: "all 0.2s ease",
+                        position: "fixed",
+                        top: "750px",
+                        left: "1600px",
+                        fontWeight: "bold",
+                    }}
+                    onMouseEnter={() => setHoverNext(true)}
+                    onMouseLeave={() => setHoverNext(false)}
+                    onClick={() => navigate("/pages/assembly")}
+                >
+                    Next Page
+                </button> {/* This Button navigates to assembly page */}
+                <button
+                    style={{
+                        color: hoverLast ? "black" : "rgb(66, 154, 255)",
+                        fontSize: "18px",
+                        border: "2px solid rgb(66, 154, 255)",
+                        background: hoverLast ? "rgb(66, 154, 255)" : "transparent",
+                        padding: "12px 30px",
+                        borderRadius: "6px",
+                        cursor: "pointer",
+                        transition: "all 0.2s ease",
+                        position: "fixed",
+                        top: "750px",
+                        left: "140px",
+                        fontWeight: "bold",
+                    }}
+                    onMouseEnter={() => setHoverLast(true)}
+                    onMouseLeave={() => setHoverLast(false)}
+                    onClick={() => navigate("/pages/typescript")}
+                >
+                    Last Page
+                </button> {/*This Button navigates to the last page*/}
             </div>
         </>
     );
